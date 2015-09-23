d06084abb22c9951905e2d0bb374629a754f2f60
Fonserbc <fonserbc@gmail.com>
2015-09-23 22:36:09 +0200

    Added soing

---
+++ GearMaster/Assets/Scripts/WorldCanvasScaler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldCanvasScaler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		float ratio = (float)Screen.width / (float)Screen.height;
+
+		GetComponent<RectTransform> ().sizeDelta = new Vector2 (GetComponent<RectTransform> ().sizeDelta.y * ratio, GetComponent<RectTransform> ().sizeDelta.y);
+	}
+}
